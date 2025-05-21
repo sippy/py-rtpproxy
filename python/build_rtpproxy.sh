b64ef7ed17b709fdb8efd20848015743d5f94394
Maksym Sobolyev <sobomax@sippysoft.com>
2025-05-20 19:20:02 -0700

    Separate build options for libraries and module itself.

--- python/build_rtpproxy.sh
+++ python/build_rtpproxy.sh
@@ -19,30 +19,35 @@ else
   GMAKE=make
 fi
 
+CFLAGS_opt="-O1 -pipe -flto"
 CFLAGS="-O0 -g3 -pipe -flto"
 if [ ! -z "${ARCH_CFLAGS}" ]
 then
+  CFLAGS_opt="${CFLAGS_opt} ${ARCH_CFLAGS}"
   CFLAGS="${CFLAGS} ${ARCH_CFLAGS}"
 fi
-LDFLAGS="-O0 -g3 -flto"
+LDFLAGS_opt="-O2 -flto"
+LDFLAGS="-O0 -flto"
 
 if [ -z "${NO_BUILD_OSSL}" ]
 then
   cd ${MYDIR}/../openssl
-  CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" ./Configure no-shared no-module no-dso \
+  CFLAGS="${CFLAGS_opt}" LDFLAGS="${LDFLAGS_opt}" ./Configure no-shared no-module no-dso \
    no-tests no-apps no-unit-test no-quic no-docs --prefix="${BDIR}" --libdir=lib \
    ${OPENSSL_CONFIGURE_ARGS}
   make all
   make install_sw
 fi
 
+CFLAGS_opt="${CFLAGS_opt} -I${BDIR}/include"
 CFLAGS="${CFLAGS} -I${BDIR}/include"
+LDFLAGS_opt="${LDFLAGS_opt} -L${BDIR}/lib"
 LDFLAGS="${LDFLAGS} -L${BDIR}/lib"
 
 if [ -z "${NO_BUILD_SRTP}" ]
 then
   cd ${MYDIR}/../libsrtp
-  CFLAGS="${CFLAGS}" LDFLAGS="${LDFLAGS}" ./configure \
+  CFLAGS="${CFLAGS_opt}" LDFLAGS="${LDFLAGS_opt}" ./configure \
    --prefix="${BDIR}" --enable-static --disable-shared --enable-openssl \
    --with-openssl-dir="${BDIR}"
   ${GMAKE} libsrtp2.a
